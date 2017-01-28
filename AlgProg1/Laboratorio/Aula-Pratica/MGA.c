40175e6dc5953508b12a9dbe57c4e650a615c45a
Magoxter <will.lbx@hotmail.com>
2017-01-28 19:04:14 -0300

    Media Geral Acumulada

---
+++ AlgProg1/Laboratorio/Aula-Pratica/MGA.c
@@ -0,0 +1,19 @@
+    /* MGA */
+#include <stdio.h>
+#include <string.h>
+#define MAX 100
+
+int main (void) {
+  int    n,i,
+         notas[MAX];
+  double soma,mga;
+   soma = 0.0;
+    scanf("%d", &n);
+   for (i = 0; i < n; i++)
+     scanf("%d", &notas[i]);
+   for (i = 0; i < n; i++)
+     soma += v[i];
+    mga = soma / n;
+   printf("%lf\n", mga);
+ return 0;
+}
