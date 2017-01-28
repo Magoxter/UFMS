748cf67308bbaece385ddef710e33749cf3a746a
Magoxter <will.lbx@hotmail.com>
2017-01-28 18:12:46 -0300

    Soma

---
+++ AlgProg1/Laboratorio/Aula-Pratica/Soma.c
@@ -0,0 +1,13 @@
+    /* Soma */
+#include <stdio.h>
+
+int main (void) {
+  int num1,
+      num2,
+      soma;
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+  soma = num1 + num2;
+   printf("%d\n", soma);
+ return 0;
+}
