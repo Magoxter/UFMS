13656125c94c2aa1713b5068d9fb7104ce3f4694
Magoxter <will.lbx@hotmail.com>
2017-01-29 09:50:33 -0300

    Hello Word

---
+++ AlgProg1/Laboratorio/Aula-Pratica/Hello-Word.c
@@ -0,0 +1,17 @@
+// Programa: oi.c
+// Programador: Willian Xavier
+// Data: 03/06/2016
+// Este programa imprime Oi na tela do computador
+// Declaração das Bibliotecas Utilizadas
+   #include<stdio.h>
+
+// inicio da função principal
+   int main(void) {
+
+// Passo 1. Imprima oi
+   printf("Oi\n");
+   printf("UFMS\n");
+   printf("Facom\n");
+   
+   return 0;
+   }   // fim da função principal
