feb2266abbd0a545b4c6d85c18807560deae7bab
Magoxter <will.lbx@hotmail.com>
2017-01-31 15:53:55 -0300

    Multiplicacao Escalar

---
+++ VGA/Vetores/MultEsc.c
@@ -0,0 +1,25 @@
+	/* Multiplicacao Escalar */
+// Este Programa Computa a Multiplicacao 
+// de um Numero Real por um Vetor
+
+#include <stdio.h>
+#define MAX 1000
+
+int main (void) {
+  float escalar,
+  		v[MAX];
+  int 	n,i;
+  	 n = 3;
+   printf("Escalar: ");
+  	scanf("%f", &real);
+   printf("Vetor: ");
+  for (i = 0; i < n; i++)
+	scanf("%f", &v[i]);
+  for (i = 0; i < n; i++)
+  	 v[i] = v[i] * real;
+//	printf("Vetor: \n");
+  for (i = 0; i < n; i++)
+  	printf("%.0f ", v[i]);
+  	printf("\n");
+ return 0;
+}
