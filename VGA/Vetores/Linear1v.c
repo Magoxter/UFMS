8041a13d6a5a0a929afc9abd7c3957f920f32aa9
Magoxter <will.lbx@hotmail.com>
2017-01-31 16:50:59 -0300

    Linearidade Vetorial

---
+++ VGA/Vetores/Linear1v.c
@@ -0,0 +1,22 @@
+	/* Linearidade */
+// Este Programa Computa a
+// Linearidade de 1Vetor
+
+#include <stdio.h>
+#define MAX 1000
+
+int main (void) {
+  int	n,i;
+  float	v[MAX];
+     n = 3;
+    printf("Vetor v: ");
+  for (i = 0; i < n; i++)
+     scanf("%f", &v[i]);
+   if (v[0] == 0 &&
+   	   v[1] == 0 &&
+   	   v[2] == 0)
+   	  printf("LD\n");
+   else
+  	  printf("LI\n");
+ return 0;
+}
