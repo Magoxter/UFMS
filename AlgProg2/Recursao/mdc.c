b6da1d6cac168cdbd97c5016271f9bbbf5486071
Will Xavier <will.lbx@hotmail.com>
2017-08-25 14:16:22 -0400

    Algoritmo de Euclides

---
+++ AlgProg2/Recursao/mdc.c
@@ -0,0 +1,25 @@
+	/* Euclides */
+#include <stdio.h>
+
+int Algrithm (int,int);
+
+int main (void) {
+  int n,r,k;
+   scanf("%d", &r);
+ while (r--) {
+   scanf("%d", &n);
+   scanf("%d", &k);
+  printf("%d\n", Algrithm(n,k));
+ }
+ return 0;
+}
+
+int Algrithm (int a, int b) {
+  int mdc,r;
+   r = a % b;
+ if (!r)
+ 	 mdc = b;
+ else
+ 	 mdc = Algrithm(b,a%b);
+ return mdc;
+}
