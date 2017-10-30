34ff615d82468032bf6234893f65a12a525fd739
Will Xavier <will.lbx@hotmail.com>
2017-10-30 19:25:20 -0300

    Soma

---
+++ AlgProg1/Laboratorio/Exercicios/Funcoes/Soma.c
@@ -0,0 +1,37 @@
+    /* Soma */
+#include <stdio.h>
+
+int leia(void);
+void print(int);
+
+int main (void) {
+
+    int num1,
+        num2,
+        soma;
+
+    num1 = leia();
+    num2 = leia();
+
+    soma = num1 + num2;
+
+    print(soma);
+
+    return 0;
+}
+
+int leia(void) {
+
+    int num;
+
+    scanf("%d", &num);
+
+    return num;
+}
+
+void print(int soma) {
+
+    printf("%d", soma);
+
+    return;
+}
